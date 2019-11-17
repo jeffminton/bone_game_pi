57f3808a9a2850b3f6e029f30cfcb04305f07c8b
Jeffrey Minton <jeffrey.minton@gmail.com>
2019-11-17 15:01:24 -0800

    Loop music

--- bone_game_pi.py
+++ bone_game_pi.py
@@ -216,7 +216,7 @@ if __name__ == '__main__':
                     bone_game.set_led(bone_game.LETTER_LED_MAP[bone_game.selected_bone_name()], green)
                     #Play correct sound
                     pygame.mixer.music.load('sounds/correct_clapping.wav')
-                    pygame.mixer.music.play(loops=1)
+                    pygame.mixer.music.play(1)
                     while pygame.mixer.music.get_busy() == True:
                         pass
                     
@@ -231,7 +231,7 @@ if __name__ == '__main__':
                     bone_game.set_led(bone_game.LETTER_LED_MAP[bone_game_key.ANSWER_KEY[bone_game.selected_bone()]], green)
                     #play wrong sound
                     pygame.mixer.music.load('sounds/wrong.wav')
-                    pygame.mixer.music.play(loops=1)
+                    pygame.mixer.music.play(1)
                     while pygame.mixer.music.get_busy() == True:
                         pass
 
